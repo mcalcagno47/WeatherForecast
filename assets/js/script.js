0e0f20b00a5c4b4991a164934f8bd56950920e13
Mark Calcagno <mcalcagn@gmail.com>
2022-11-05 18:40:49 -0500

    Javascript should be working, but 401 error is coming up.  Cheated and put lat and lon on page. Will remove before posting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,65 @@
-var cityInput = document.querySelector("#city-input");
-var searchButton = document.querySelector("#btn");
-var savedCity = document.querySelector("#saved-city")
+const app = {
+    init: () => {
+        document
+            .querySelector('#search-btn')
+            .addEventListener('click', app.fetchWeather);
+    },
 
+    // (ev) is the event of clicking the search button
+    fetchWeather: (ev) => {
+        var lat = document.getElementById('latitude').value;
+        var lon = document.getElementById('longitude').value;
+        var key = '99c3f0b955fb6ab95dfe2df6ef3c94ea';
+        var lang = 'en';
+        var units = 'imperial';
+        var url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
 
+        fetch(url)
+            .then((data) => {
+                app.showWeather(data);
+            })
+    },
 
+    getLocation: (ev) => {
+        navigator.geolocation.getCurrentPosition(app.success, app.error);
+    },
 
-// API
-'api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&APPID=3a87513ddc7302050a84853267ee583c'
+    success: (position) => {
+        document.getElementById('latitude').value =
+            position.coords.latitude.toFixed(2);
+        document.getElementById('longitude').value =
+            position.coords.longitude.toFixed(2);
+    },
 
-// lat and lon
-'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=' + lat + '&lon=' + lon '&limit=5&appid=3a87513ddc7302050a84853267ee583c'
+    error: (error) => {
+        console.error(error);
+    },
 
-// City
-'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&limit=1&appid=3a87513ddc7302050a84853267ee583c'
+    showWeather: (resp) => {
+        console.log(resp);
+        let row = document.querySelector('.weather.row');
+        //resp.daily is the 5 day forecast, see line 44 with idx <= 5.
+        row.innerHTML = resp.daily
+            .map((day, idx) => {
+                if (idx <= 5) {
+                    let dt = new Date(day.dt * 1000);
+                    return `<div class="col">
+                                <div class="card">
+                                <h5 class="card-title p-2">${dt.toDateString()}</h5>
+                                    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" class="card-img-top" alt="${day.weather[0].description}"/>
+                                    <div class="card-body">
+                                        <h3 class="card-title">${day.weather[0].main}</h3>
+                                        <p class="card-text">Temperature ${day.temp.day}&deg;</p>
+                                        <p class="card-text">Humidity ${day.humidity}%</p>
+                                        <p class="card-text">Wind ${day.wind_speed}ft/s;</p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>`;
+                }
+            })
+            .join('');
+    }
+}
+
+app.init();
