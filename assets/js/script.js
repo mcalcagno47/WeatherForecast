5b497f5cd40256ba7656a00dbfb9135d71341b8c
Mark Calcagno <mcalcagn@gmail.com>
2022-11-06 17:35:49 -0600

    5 day forecast works

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,17 @@ const app = {
             .addEventListener('click', app.fetchWeather);
     },
 
-    // (ev) is the event of clicking the search button
     fetchWeather: (ev) => {
         let lat = document.getElementById('latitude').value;
         let lon = document.getElementById('longitude').value;
         let key = '99c3f0b955fb6ab95dfe2df6ef3c94ea';
         let lang = 'en';
         let units = 'imperial';
-        let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+        let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
 
         fetch(url)
-            .then((data) => {
-                app.showWeather(data);
-            })
+            .then((response) => response.json())
+            .then((data) => app.showWeather(data));
     },
 
     getLocation: (ev) => {
@@ -37,12 +35,13 @@ const app = {
 
     showWeather: (resp) => {
         let row = document.querySelector('.weather.row');
-        let respJson = resp.json()
-        console.log(respJson);
-        // resp.daily is the 5 day forecast, see line 44 with idx <= 5.
-        row.innerHTML = respJson
-            .each((day, idx) => {
-                if (idx <= 5) {
+
+        let filteredList = resp.list.filter(function (v, index) { return [0, 8, 16, 24, 32, 39].includes(index) })
+
+        row.innerHTML = filteredList
+            .map((day, idx) => {
+                console.log(day)
+                if (idx <= 4) {
                     let dt = new Date(day.dt * 1000);
                     return `<div class="col">
                                 <div class="card">
@@ -50,9 +49,9 @@ const app = {
                                     <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" class="card-img-top" alt="${day.weather[0].description}"/>
                                     <div class="card-body">
                                         <h3 class="card-title">${day.weather[0].main}</h3>
-                                        <p class="card-text">Temperature ${day.temp.day}&deg;</p>
-                                        <p class="card-text">Humidity ${day.humidity}%</p>
-                                        <p class="card-text">Wind ${day.wind_speed}ft/s;</p>
+                                        <p class="card-text">Temperature ${day.main.temp}&deg;</p>
+                                        <p class="card-text">Humidity ${day.main.humidity}%</p>
+                                        <p class="card-text">Wind ${day.wind.speed}ft/s;</p>
                                     </div>
                                 </div>
                                 </div>
@@ -64,6 +63,3 @@ const app = {
 }
 
 app.init();
-
-
-// let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
